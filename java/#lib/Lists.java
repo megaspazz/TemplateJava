521f4687df9e34d60c9924ce8a9d1eeb1f3e617a
Eric Pan <erzpan@gmail.com>
2022-09-26 09:09:24 -0700

    Add LongList (forked from IntList).

--- java/#lib/Lists.java
+++ java/#lib/Lists.java
@@ -135,4 +135,139 @@ public class Lists {
 			}
 		}
 	}
+
+	public static class LongList {
+		public long[] arr;
+		public int pos;
+
+		public LongList(int capacity) {
+			this.arr = new long[capacity];
+		}
+
+		public LongList() {
+			this(10);
+		}
+
+		public LongList(LongList other) {
+			this.arr = Arrays.copyOfRange(other.arr, 0, other.pos);
+			this.pos = other.pos;
+		}
+
+		public void add(long x) {
+			if (pos >= arr.length) {
+				resize(arr.length << 1);
+			}
+			arr[pos++] = x;
+		}
+
+		public void insert(int i, long x) {
+			if (pos >= arr.length) {
+				resize(arr.length << 1);
+			}
+			System.arraycopy(arr, i, arr, i + 1, pos++ - i);
+			arr[i] = x;
+		}
+
+		public long get(int i) {
+			return arr[i];
+		}
+
+		public void set(int i, long x) {
+			arr[i] = x;
+		}
+
+		public void clear() {
+			pos = 0;
+		}
+
+		public int size() {
+			return pos;
+		}
+
+		public boolean isEmpty() {
+			return pos == 0;
+		}
+
+		public long last() {
+			return arr[pos - 1];
+		}
+
+		public long removeLast() {
+			return arr[--pos];
+		}
+
+		public void push(int x) {
+			add(x);
+		}
+
+		public long pop() {
+			return removeLast();
+		}
+
+		public boolean remove(long x) {
+			for (int i = 0; i < pos; ++i) {
+				if (arr[i] == x) {
+					removeAt(i);
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public void removeAt(int i) {
+			System.arraycopy(arr, i + 1, arr, i, --pos - i);
+		}
+
+		public LongList subList(int fromIndex, int toIndexExclusive) {
+			LongList lst = new LongList();
+			lst.arr = Arrays.copyOfRange(arr, fromIndex, toIndexExclusive);
+			lst.pos = toIndexExclusive - fromIndex;
+			return lst;
+		}
+
+		public void forEach(LongConsumer consumer) {
+			for (int i = 0; i < pos; ++i) {
+				consumer.accept(arr[i]);
+			}
+		}
+
+		public long[] toArray() {
+			return Arrays.copyOf(arr, pos);
+		}
+
+		public static LongList of(int... items) {
+			LongList lst = new LongList(items.length);
+			System.arraycopy(items, 0, lst.arr, 0, items.length);
+			lst.pos = items.length;
+			return lst;
+		}
+
+		public String join(CharSequence sep) {
+			StringBuilder sb = new StringBuilder();
+			joinToBuffer(sb, sep);
+			return sb.toString();
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			sb.append('[');
+			joinToBuffer(sb, ", ");
+			sb.append(']');
+			return sb.toString();
+		}
+
+		private void resize(int newCapacity) {
+			arr = Arrays.copyOf(arr, newCapacity);
+		}
+
+		private void joinToBuffer(StringBuilder sb, CharSequence sep) {
+			for (int i = 0; i < pos; ++i) {
+				if (i > 0) {
+					sb.append(sep);
+				}
+				sb.append(arr[i]);
+			}
+		}
+	}
 }
