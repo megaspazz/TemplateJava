cc91655869a865f52c47ed1236439dfc2abced3d
Eric Pan <erzpan@gmail.com>
2022-09-27 17:08:38 -0700

    Add cache for IntKey like how Java Integer works.

--- java/#lib/Hashing.java
+++ java/#lib/Hashing.java
@@ -1,10 +1,8 @@
 public class Hashing {
 	public static class IntKey {
-		private static final int ADD_MIX = mulberry32((int)System.nanoTime());
-		
 		public int value;
 
-		public IntKey(int value) {
+		private IntKey(int value) {
 			this.value = value;
 		}
 
@@ -29,6 +27,24 @@ public class Hashing {
 			z = (z ^ (z >>> 15)) * (z | 1);
 			z ^= z + (z ^ (z >>> 7)) * (z | 61);
 			return z ^ (z >>> 14);
+		}
+
+		public static IntKey of(int value) {
+			if (CACHE_MIN <= value && value < CACHE_MAX) {
+				return CACHE[value - CACHE_MIN];
+			}
+			return new IntKey(value);
+		}
+
+		private static final int ADD_MIX = mulberry32((int)System.nanoTime());
+
+		private static final int CACHE_MIN = -256;
+		private static final int CACHE_MAX = 256;
+		private static final IntKey[] CACHE = new IntKey[CACHE_MAX - CACHE_MIN];
+		static {
+			for (int i = CACHE_MIN; i < CACHE_MAX; ++i) {
+				CACHE[i - CACHE_MIN] = new IntKey(i);
+			}
 		}
 	}
 }
