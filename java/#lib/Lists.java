803747622a3d45396bd4604c243e0baf88d8e1e7
Eric Pan <erzpan@gmail.com>
2022-09-30 15:09:11 -0700

    Fix IntDeque logic and add LongDeque.

--- java/#lib/Lists.java
+++ java/#lib/Lists.java
@@ -275,15 +275,15 @@ public class Lists {
 		private int[] arr;
 		private int off;
 		private int len;
-		
+
 		public IntDeque() {
 			this(12);
 		}
-		
+
 		public IntDeque(int capacity) {
 			this.arr = new int[capacity];
 		}
-		
+
 		public void addFirst(int x) {
 			if (len == arr.length) {
 				increaseCapacity();
@@ -294,7 +294,7 @@ public class Lists {
 			arr[--off] = x;
 			++len;
 		}
-		
+
 		public void addLast(int x) {
 			if (len == arr.length) {
 				increaseCapacity();
@@ -303,69 +303,69 @@ public class Lists {
 			arr[idx] = x;
 			++len;
 		}
-		
-		public int peekFirst() {
+
+		public long peekFirst() {
 			return arr[off];
 		}
-		
-		public int peekLast() {
-			int idx = index(off + len);
+
+		public long peekLast() {
+			int idx = index(off + len - 1);
 			return arr[idx];
 		}
-		
-		public int removeFirst() {
-			int ans = peekFirst();
+
+		public long removeFirst() {
+			long ans = peekFirst();
 			off = index(off + 1);
 			--len;
 			return ans;
 		}
-		
-		public int removeLast() {
-			int ans = peekLast();
+
+		public long removeLast() {
+			long ans = peekLast();
 			--len;
 			return ans;
 		}
-		
+
 		public void add(int x) {
 			addLast(x);
 		}
-		
+
 		public void offer(int x) {
 			addLast(x);
 		}
-		
-		public int poll() {
+
+		public long poll() {
 			return removeFirst();
 		}
-		
-		public int peekQueue() {
+
+		public long peekQueue() {
 			return peekFirst();
 		}
-		
+
 		public void push(int x) {
 			addLast(x);
 		}
-		
-		public int pop() {
+
+		public long pop() {
 			return removeLast();
 		}
-		
-		public int peekStack() {
+
+		public long peekStack() {
 			return peekLast();
 		}
-		
-		public int peek() {
+
+		public long peek() {
 			return peekFirst();
 		}
-		
-		public int get(int i) {
+
+		public long get(int i) {
 			if (i >= len) {
 				throw new ArrayIndexOutOfBoundsException(String.format("index %d out of range [0, %d)", i, len));
 			}
 			int idx = index(i + off);
 			return arr[idx];
 		}
-		
+
 		public void set(int i, int x) {
 			if (i >= len) {
 				throw new ArrayIndexOutOfBoundsException(String.format("index %d out of range [0, %d)", i, len));
@@ -373,15 +373,24 @@ public class Lists {
 			int idx = index(i + off);
 			arr[idx] = x;
 		}
-		
+
 		public int size() {
 			return len;
 		}
-		
+
 		public boolean isEmpty() {
 			return size() == 0;
 		}
-		
+
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			sb.append('[');
+			printToBuffer(sb, ", ");
+			sb.append(']');
+			return sb.toString();
+		}
+
 		private void increaseCapacity() {
 			int[] next = new int[arr.length << 1];
 			int endLen = arr.length - off;
@@ -390,77 +399,187 @@ public class Lists {
 			arr = next;
 			off = 0;
 		}
-		
+
 		private int index(int i) {
 			if (i >= arr.length) {
 				i -= arr.length;
+			} else if (i < 0) {
+				i += arr.length;
 			}
 			return i;
 		}
+
+		private void printToBuffer(StringBuilder sb, CharSequence sep) {
+			for (int i = 0; i < len; ++i) {
+				if (i > 0) {
+					sb.append(sep);
+				}
+				sb.append(get(i));
+			}
+		}
+
+		public static IntDeque of(int... arr) {
+			IntDeque deq = new IntDeque();
+			for (int x : arr) {
+				deq.add(x);
+			}
+			return deq;
+		}
 	}
 
-	/**
-	 * This is an priority queue containing int values in descending order.
-	 *   - All operations run in amortized O(1) time.
-	 *   - However, it can only offer values that are greater-than-or-equal-to the last value of poll().
-	 *   - Before the first poll() call, any values can be inserted.
-	 */
-	public static class AscendingDiscretePriorityQueue<T> {
-		private int idx;
-		private ArrayList<ArrayDeque<T>> queues;
-		private IntConverter<T> converter;
+	public static class LongDeque {
+		private long[] arr;
+		private int off;
+		private int len;
 
-		public AscendingDiscretePriorityQueue(IntConverter<T> converter) {
-			this.queues = new ArrayList<>();
-			this.converter = converter;
+		public LongDeque() {
+			this(12);
 		}
 
-		public boolean offer(T item) {
-			int x = converter.toInt(item);
-			if (x < idx) {
-				return false;
+		public LongDeque(int capacity) {
+			this.arr = new long[capacity];
+		}
+
+		public void addFirst(long x) {
+			if (len == arr.length) {
+				increaseCapacity();
 			}
-			getOrCreate(x).offer(item);
-			return true;
+			if (off == 0) {
+				off = arr.length;
+			}
+			arr[--off] = x;
+			++len;
 		}
 
-		public T peek() {
-			return nextQueue().peek();
+		public void addLast(long x) {
+			if (len == arr.length) {
+				increaseCapacity();
+			}
+			int idx = index(off + len);
+			arr[idx] = x;
+			++len;
 		}
 
-		public T poll() {
-			return nextQueue().poll();
+		public long peekFirst() {
+			return arr[off];
+		}
+
+		public long peekLast() {
+			int idx = index(off + len - 1);
+			return arr[idx];
+		}
+
+		public long removeFirst() {
+			long ans = peekFirst();
+			off = index(off + 1);
+			--len;
+			return ans;
+		}
+
+		public long removeLast() {
+			long ans = peekLast();
+			--len;
+			return ans;
+		}
+
+		public void add(long x) {
+			addLast(x);
+		}
+
+		public void offer(long x) {
+			addLast(x);
+		}
+
+		public long poll() {
+			return removeFirst();
+		}
+
+		public long peekQueue() {
+			return peekFirst();
+		}
+
+		public void push(long x) {
+			addLast(x);
+		}
+
+		public long pop() {
+			return removeLast();
+		}
+
+		public long peekStack() {
+			return peekLast();
+		}
+
+		public long peek() {
+			return peekFirst();
+		}
+
+		public long get(int i) {
+			if (i >= len) {
+				throw new ArrayIndexOutOfBoundsException(String.format("index %d out of range [0, %d)", i, len));
+			}
+			int idx = index(i + off);
+			return arr[idx];
+		}
+
+		public void set(int i, long x) {
+			if (i >= len) {
+				throw new ArrayIndexOutOfBoundsException(String.format("index %d out of range [0, %d)", i, len));
+			}
+			int idx = index(i + off);
+			arr[idx] = x;
+		}
+
+		public int size() {
+			return len;
 		}
 
 		public boolean isEmpty() {
-			return nextQueue() == null;
+			return size() == 0;
 		}
 
-		private ArrayDeque<T> nextQueue() {
-			while (idx < queues.size()) {
-				ArrayDeque<T> q = queues.get(idx);
-				if (q != null && !q.isEmpty()) {
-					return q;
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			sb.append('[');
+			printToBuffer(sb, ", ");
+			sb.append(']');
+			return sb.toString();
+		}
+
+		private void increaseCapacity() {
+			long[] next = new long[arr.length << 1];
+			int endLen = arr.length - off;
+			System.arraycopy(arr, off, next, 0, endLen);
+			System.arraycopy(arr, 0, next, endLen, off);
+			arr = next;
+			off = 0;
+		}
+
+		private int index(int i) {
+			if (i >= arr.length) {
+				i -= arr.length;
+			} else if (i < 0) {
+				i += arr.length;
+			}
+			return i;
+		}
+
+		private void printToBuffer(StringBuilder sb, CharSequence sep) {
+			for (int i = 0; i < len; ++i) {
+				if (i > 0) {
+					sb.append(sep);
 				}
-				++idx;
+				sb.append(get(i));
 			}
-			return null;
 		}
 
-		private ArrayDeque<T> getOrCreate(int i) {
-			while (queues.size() <= i) {
-				queues.add(null);
+		public static LongDeque of(long... arr) {
+			LongDeque deq = new LongDeque();
+			for (long x : arr) {
+				deq.add(x);
 			}
-			ArrayDeque<T> q = queues.get(i);
-			if (q == null) {
-				q = new ArrayDeque<>();
-				queues.set(i, q);
-			}
-			return q;
-		}
-
-		public static interface IntConverter<T> {
-			public int toInt(T item);
+			return deq;
 		}
 	}
 }
