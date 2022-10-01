b1c2f3ecb2dd54f23d06f7b68bbafec8e8c8e547
Eric Pan <erzpan@gmail.com>
2022-10-01 08:04:18 -0700

    Add hack-resistant LongKey.

--- java/#lib/Hashing.java
+++ java/#lib/Hashing.java
@@ -47,4 +47,53 @@ public class Hashing {
 			}
 		}
 	}
+
+	public static class LongKey {
+		public long value;
+
+		private LongKey(long value) {
+			this.value = value;
+		}
+
+		@Override
+		public int hashCode() {
+			return Long.hashCode(splitmix64(value + ADD_MIX));
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			LongKey other = (LongKey) obj;
+			return value == other.value;
+		}
+
+		@Override
+		public String toString() {
+			return Long.toString(value);
+		}
+
+		private static final long splitmix64(long x) {
+			long z = x + 0x9E3779B97F4A7C15L;
+			z = (z ^ (z >>> 30)) * 0xBF58476D1CE4E5B9L;
+			z = (z ^ (z >>> 27)) * 0x94D049BB133111EBL;
+			return z ^ (z >>> 31);
+		}
+
+		public static LongKey of(long value) {
+			if (CACHE_MIN <= value && value < CACHE_MAX) {
+				return CACHE[(int) (value - CACHE_MIN)];
+			}
+			return new LongKey(value);
+		}
+
+		private static final long ADD_MIX = splitmix64(System.nanoTime());
+
+		private static final int CACHE_MIN = -256;
+		private static final int CACHE_MAX = 256;
+		private static final LongKey[] CACHE = new LongKey[CACHE_MAX - CACHE_MIN];
+		static {
+			for (int i = CACHE_MIN; i < CACHE_MAX; ++i) {
+				CACHE[i - CACHE_MIN] = new LongKey(i);
+			}
+		}
+	}
 }
