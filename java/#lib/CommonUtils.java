44ca3a409b0cc80509b064f97bf73df5e904c298
Eric Pan <erzpan@gmail.com>
2022-10-07 17:19:33 -0700

    Update count maps with total.

--- java/#lib/CommonUtils.java
+++ java/#lib/CommonUtils.java
@@ -4,15 +4,20 @@ public class CommonUtils {
 	/**
 	 * Counts the frequency of objects.
 	 * Change to extend TreeMap instead, if ordering of objects is required.
+	 * 
+	 * NOTE:  If `total` is needed, only use `increment(...)` for updates.
 	 */
 	public static class CountMapInt<T> extends HashMap<T, Integer> {
 		private static final long serialVersionUID = -1501598139835601959L;
+
+		public int total;
 
 		public int getCount(T k) {
 			return getOrDefault(k, 0);
 		}
 
 		public void increment(T k, int v) {
+			total += v;
 			int next = getCount(k) + v;
 			if (next == 0) {
 				remove(k);
@@ -33,15 +38,20 @@ public class CommonUtils {
 	/**
 	 * Counts the frequency of objects.
 	 * Change to extend TreeMap instead, if ordering of objects is required.
+	 * 
+	 * NOTE:  If `total` is needed, only use `increment(...)` for updates.
 	 */
 	public static class CountMapLong<T> extends HashMap<T, Long> {
 		private static final long serialVersionUID = -9079906779955923767L;
+
+		public long total;
 
 		public long getCount(T k) {
 			return getOrDefault(k, 0L);
 		}
 
 		public void increment(T k, long v) {
+			total += v;
 			long next = getCount(k) + v;
 			if (next == 0) {
 				remove(k);
